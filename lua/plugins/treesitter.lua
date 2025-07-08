bb2bb242f55cc244a54b09e107f6c4635ff2daf8
Evan Snoswell <evan.snoswell@gmail.com>
2025-07-08 15:13:16 +0100

    ensure folding definitely works

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  config = function()
-    vim.cmd('TSUpdate')
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		vim.cmd("TSUpdate")
+	end,
+	setup = {
+		ensure_installed = { "c", "lua", "markdown", "markdown_inline", "typescript", "javascript", "svelte" },
+	},
 }
-
