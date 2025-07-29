264d38474d549d2dc6405aa3dc1596568f11ca13
Evan Snoswell <evan.snoswell@gmail.com>
2025-07-29 11:28:51 +0100

    don't block on startup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		vim.cmd("TSUpdate")
-	end,
+	--config = function()
+	--	vim.cmd("TSUpdate")
+	--end,
 	setup = {
 		ensure_installed = { "c", "lua", "markdown", "markdown_inline", "typescript", "javascript", "svelte" },
 	},
